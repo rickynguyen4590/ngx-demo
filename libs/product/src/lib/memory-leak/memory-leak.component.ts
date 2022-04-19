15c6058da75cebf819ee2c84e4d3179148475a09
Ricky Nguyen <ricky.nguyen@noria.no>
2022-04-19 11:06:48 +0700

    (feat) add routing

---
+++ libs/product/src/lib/memory-leak/memory-leak.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'ng13-memory-leak',
+  templateUrl: './memory-leak.component.html',
+})
+export class MemoryLeakComponent implements OnInit {
+}
--- libs/rxjs/src/lib/memory-leak/memory-leak.component.ts
+++ libs/rxjs/src/lib/memory-leak/memory-leak.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, of, switchMap, interval } from 'rxjs';
 
 @Component({
   selector: 'ng13-memory-leak',
   templateUrl: './memory-leak.component.html',
 })
-export class MemoryLeakComponent {
-  loading = false;
+export class MemoryLeakComponent implements OnInit {
+  index = 1;
+  timmer$ = interval(1000);
+  myNumber$ = new BehaviorSubject<number>(this.index);
+
+  ngOnInit() {
+    this.timmer$
+      .pipe(switchMap((x) => this.myNumber$))
+      .subscribe((x) => console.log(x));
+  }
 }
