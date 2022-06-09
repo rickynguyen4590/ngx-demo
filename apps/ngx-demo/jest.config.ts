e4871af270b98d3a3f549bec40856d443a638519
Ricky Nguyen <ricky.nguyen@noria.no>
2022-06-09 14:50:11 +0700

    REname project

--- apps/ngx-demo/jest.config.ts
+++ apps/ngx-demo/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'ng13',
+  displayName: 'ngx-demo',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/ng13',
+  coverageDirectory: '../../coverage/apps/ngx-demo',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
--- apps/ngx-demo/src/app/nav/nav.component.ts
+++ apps/ngx-demo/src/app/nav/nav.component.ts
@@ -3,11 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ng13-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
