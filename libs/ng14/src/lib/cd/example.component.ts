6f3b0492dd9db9e94c63e26ef4c8ef7f2de9948d
Ricky Nguyen <ricky.nguyen@noria.no>
2022-08-04 13:41:57 +0700

    (feat) Add update dâta on view init example

--- libs/ng14/src/lib/cd/example.component.ts
+++ libs/ng14/src/lib/cd/example.component.ts
@@ -1,37 +1,79 @@
 import { CommonModule } from "@angular/common";
-import { AfterViewInit, ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { AfterContentInit, AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, Input, OnChanges, OnInit, SimpleChanges } from "@angular/core";
 import { PushModule } from '@ngrx/component';
-import { BehaviorSubject, ReplaySubject, Subject } from "rxjs";
+import { BehaviorSubject } from "rxjs";
 
 @Component({
     standalone: true,
     selector: 'nv-cd-child',
-    template: `<p>X</p>`,
+    template: `<p>{{text()}}</p>`,
     imports: [CommonModule],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CDChildComponent implements OnInit, AfterViewInit {
+export class CDChildComponent implements OnInit, AfterViewInit, AfterContentInit, OnChanges {
     init$ = new BehaviorSubject<boolean>(false);
+
+    @Input() x: boolean | null = false;
 
     constructor() {
         this.init$.subscribe(x => { console.log('Status', x); });
     }
 
+    ngOnChanges(): void {
+        console.log('child: ngOnChanges');
+    }
+
     ngOnInit() {
         this.init$.next(true);
+        console.log('child: ngOnInit');
+    }
+
+    ngAfterContentInit(): void {
+        this.init$.next(true);
+        console.log('child: ngAfterContentInit');
     }
 
     ngAfterViewInit(): void {
-        return;
+        this.init$.next(true);
+        console.log('child: ngAfterViewInit');
+    }
+
+    text() {
+        console.log('child: CD');
+        return ''
     }
 }
 
 @Component({
     standalone: true,
     selector: 'nv-cd-parent',
-    template: `<p>Child status: {{child.init$ | async  | json}}</p><nv-cd-child #child></nv-cd-child>`,
+    template: `<p>Child status: {{child.init$ | async  | json}} {{text()}}</p><nv-cd-child #child [x]="child.init$ | async" ></nv-cd-child>`,
     imports: [CDChildComponent, CommonModule, PushModule],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CDParentComponent {
+export class CDParentComponent implements OnInit, AfterViewInit, AfterContentInit, OnChanges {
+    @Input() y = false;
+
+    cd = inject(ChangeDetectorRef);
+
+    ngOnChanges(): void {
+        console.log('parent: ngOnChanges');
+    }
+
+    ngOnInit() {
+        console.log('parent: ngOnInit');
+    }
+
+    ngAfterContentInit(): void {
+        console.log('parent: ngAfterContentInit');
+    }
+
+    ngAfterViewInit(): void {
+        console.log('parent: ngAfterViewInit');
+    }
+
+    text() {
+        console.log('parent: CD');
+        return '';
+    }
 }
